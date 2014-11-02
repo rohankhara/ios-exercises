24152b43c2fcef8baefe77ccc05c404aff18bcd8
Rohan Khara <khararohan@gmail.com>
2014-11-02 21:17:59 +0530

    Commit on Nov 2 for ckp #11 Loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,53 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    
+    
+    if (number < otherNumber)
+        
+    {
+     
+       // NSRange numberRange = NSMakeRange(number, otherNumber);
+
+        // NSIndexSet *returnValue = [NSIndexSet indexSetWithIndexesInRange: NSMakeRange(number, otherNumber)];
+        // NSString *tobereturned = [NSString stringWithFormat:@"%i", (int)returnValue];
+        NSMutableArray *arrayToBeReturned = [NSMutableArray array];
+        for (NSInteger i = number; i <= otherNumber; i++)
+        {
+        
+            [arrayToBeReturned addObject:[NSNumber numberWithInteger:i]];
+            
+        }
+        
+        
+        int len = sizeof(arrayToBeReturned);
+        NSMutableString *stringToBeReturned = [NSMutableString string];
+        
+        for (NSInteger j= 0; j<len; j++)
+    
+            
+        {
+            [stringToBeReturned appendFormat:@"%i", arrayToBeReturned[j]];
+        }
+        
+        return stringToBeReturned;
+        
+    }
+    
+    
+    if (number > otherNumber);
+    
+    {
+    }
+    
+    
+    if (number == otherNumber)
+    {
+        NSString *tobereturned = [NSString stringWithFormat:@"%i", (int)number];
+        return  tobereturned;
+    }
+    return nil;
 }
 
 @end
