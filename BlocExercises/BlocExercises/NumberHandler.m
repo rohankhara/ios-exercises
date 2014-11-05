bb5eefe1b8dc423528923eab2bab09260a333b3f
Rohan Khara <khararohan@gmail.com>
2014-11-05 12:18:17 +0530

    commit for ckp #14 objects on nov 5

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,50 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSNumber *rohan = @([number floatValue] * 2);
+        return rohan;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *tempArray = [NSMutableArray array];
+    
+    if (number > otherNumber)
+        
+    {
+    
+        int y = number;
+        number = otherNumber;
+        otherNumber = y;
+    
+    }
+    
+     if (number < otherNumber)
+         
+     {
+         
+         for (int z= number; z<= otherNumber;z++)
+         
+         {
+             [tempArray addObject: @(z)];
+         
+         }
+             
+     }
+    NSSortDescriptor *rohanDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [tempArray sortUsingDescriptors:@[rohanDescriptor]];
+    
+    return tempArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    
+    NSMutableArray *tempArray = [arrayOfNumbers mutableCopy];
+    
+    NSSortDescriptor *rohanDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [tempArray sortUsingDescriptors:@[rohanDescriptor]];
+    NSInteger x = [[tempArray objectAtIndex:0] intValue];
+    return x;
 }
 
 @end
